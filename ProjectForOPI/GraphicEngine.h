990b9fefcd8a0615c3d50fd3b6044bc8f4edc37e
Dimain <dima123.zample@gmail.com>
2021-12-15 18:17:21 +0300

    Graphic is work

--- ProjectForOPI/GraphicEngine.h
+++ ProjectForOPI/GraphicEngine.h
@@ -5,37 +5,70 @@
 
 using namespace std;
 
+class GameObject
+{
+public:
+
+	char Symbol;
+	string tag;
+
+	GameObject(char Symbol, string tag) {
+		this->Symbol = Symbol;
+		this->tag = tag;
+	}
+
+	GameObject(char Symbol) {
+		this->Symbol = Symbol;
+		this->tag = "";
+	}
+
+	GameObject() {
+		this->Symbol = ' ';
+		this->tag = "";
+	}
+
+private:
+
+};
+
 class GameMap
 {
 public:
 
-	char defaultSymbol;
+	GameObject defaultObject;
 
-	GameMap(Vector2 size, char defaultSymbol) {
+	GameMap(Vector2 size, GameObject defaultObject) {
 		this->size = size;
-		this->defaultSymbol = defaultSymbol;
+		this->defaultObject = defaultObject;
 
+		for (int y = 0; y < size.y; y++)
+		{
+			Map.push_back(vector<GameObject>());
+			for (int x = 0; x < size.x; x++)
+			{
+				Map[y].push_back(defaultObject);
+			}
+		}
+	}
 
+	void draw() {
+		for (int y = 0; y < size.y; y++)
+		{
+			for (int x = 0; x < size.x; x++)
+			{
+				cout << Map[y][x].Symbol;
+			}
+			cout << endl;
+		}
 	}
 	
+	GameObject getGameObjectByMapPosition(Vector2 pos) {
+		return Map[pos.y][pos.x];
+	}
+
 private:
 
-	vector<vector<char>> charMap;
+	vector<vector<GameObject>> Map;
 	Vector2 size;
-
-	void RESIZE() {
-		
-	}
-
-};
-
-class CharObject
-{
-public:
-	
-	char Symbol;
-	string tag;
-
-private:
 
 };
