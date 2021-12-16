1fdf26ce0bfeebc974f6588936b8331a1539c41e
Dimain <dima123.zample@gmail.com>
2021-12-16 13:48:53 +0300

    GameObject -> GameObject*

--- ProjectForOPI/Source.cpp
+++ ProjectForOPI/Source.cpp
@@ -10,13 +10,22 @@ int main() {
 
 	GameMap testMap = GameMap(Vector2(10, 10), '.');
 
-	testMap.addGameObject(GameObject(Vector2(0, 0), '#', "first"));
-	testMap.addGameObject(GameObject(Vector2(1, 1), '/', "second"));
+	GameObject obj = GameObject(Vector2(0, 0), '#', "first");
+
+	testMap.addGameObject(&obj);
+	testMap.addGameObject(new GameObject(Vector2(1, 1), '/', "second"));
 
 	testMap.draw();
 
-	cout << testMap.getGameObjectById(1).position.x << endl;
-	cout << testMap.getGameObjectById(1).position.y << endl;
+	cout << "X: " << (*testMap.getGameObjectByTag("first")).position.x << endl;
+	cout << "Y: " << (*testMap.getGameObjectByTag("first")).position.y << endl;
+
+	(*testMap.getGameObjectById(0)).position.y++;
+	
+	testMap.deleteGameObjectById(1);
+
+	cout << "X: " << obj.position.x << endl;
+	cout << "Y: " << obj.position.y << endl;
 
 	testMap.draw();
 }
