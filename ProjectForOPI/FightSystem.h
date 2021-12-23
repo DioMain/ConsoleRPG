920d1f5398a44d9bacc5ae7224384543c2d1d869
Alexandr <sasha_52003@mail.ru>
2021-12-23 23:30:01 +0300

    piska

--- ProjectForOPI/FightSystem.h
+++ ProjectForOPI/FightSystem.h
@@ -18,6 +18,10 @@ namespace Game {
 	{
 		Enemy BossOfTheGYM(5000, 150, 500, "BossOfTheGYM", "BossOfTheGYM - kind on the outside, cruel on the inside, who is always ready to back you up from behind");
 		Enemy Trainer(1500, 50, 150, "Trainer", "If he is not paid, then he is very angry ...\nHe was not paid ...");
+		Enemy Security(1500, 50, 150, "Trainer", "If he is not paid, then he is very angry ...\nHe was not paid ...");
+		Enemy Manager(1500, 50, 150, "Trainer", "If he is not paid, then he is very angry ...\nHe was not paid ...");
+		Enemy Bodybuilder(1500, 50, 150, "Trainer", "If he is not paid, then he is very angry ...\nHe was not paid ...");
+		Enemy pimple(1500, 50, 150, "Trainer", "If he is not paid, then he is very angry ...\nHe was not paid ...");
 
 		Enemys.push_back(BossOfTheGYM);
 		Enemys.push_back(Trainer);
